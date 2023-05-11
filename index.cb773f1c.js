5321b6c2cc5392ecedd638bcab898646209dfbc9
mandarinca <34621787+mandarinca@users.noreply.github.com>
2023-05-11 09:18:49 +0000

    Deploying to gh-pages from @ mandarinca/goit-js-hw-10@3b43de2b239058285f4efad5b7bbb071907f4883 🚀

---
+++ index.cb773f1c.js
@@ -0,0 +1,2 @@
+!function(){var n;(n="Argentina",fetch("".concat("https://restcountries.com/v3.1/","name/").concat(n)).then((function(n){return n.json()}))).then((function(n){return console.log(n)}))}();
+//# sourceMappingURL=index.cb773f1c.js.map
---
+++ index.d2a659e5.js
@@ -0,0 +1,2 @@
+var e;(e="Argentina",fetch(`https://restcountries.com/v3.1/name/${e}`).then((e=>e.json()))).then((e=>console.log(e)));
+//# sourceMappingURL=index.d2a659e5.js.map
